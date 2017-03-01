7071c23e8f76b036dd71b23c9a33ca69f32a6c96
Chris Holt <humor4fun@users.noreply.github.com>
2017-03-01 16:23:50 -0500

    Update startup_dojo.sh
    moved nginx kickoff to the end, django must be running first
    added delay before the heartbeat tests run

--- defectdojo/startup_dojo.sh
+++ defectdojo/startup_dojo.sh
@@ -2,14 +2,19 @@
 #2017-01-10 by Chris Holt (@humor4fun)
 #gets the required bits started for Defect Dojo
 
-#kickoff the nginx server if it is not already started
-sudo service nginx start
+
 
 #start the server up in a terminal with profile settings set to HOLD the window on process exit
 gnome-terminal --working-directory=/home/dojo/django-DefectDojo --window-with-profile=cronterm --display=:0.0 --execute python manage.py runserver
 
 #give django plenty of time to start up
 sleep 5
+
+#kickoff the nginx server if it is not already started
+sudo service nginx start
+
+#give nginx and django plenty of time to start up
+sleep 10
 
 #run heartbeat tests for django and nginx
 gnome-terminal --working-directory=/home/dojo/ --window-with-profile=cronterm --display=:0.0 --execute ./status-dojo
