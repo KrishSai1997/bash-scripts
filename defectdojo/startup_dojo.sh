de9eaa698182694f9e21f5d0e54d8cf172bdd450
Chris Holt <humor4fun@users.noreply.github.com>
2017-03-01 16:09:59 -0500

    Update startup_dojo.sh
    added nginx kicker
    added heartbeat script tests

--- defectdojo/startup_dojo.sh
+++ defectdojo/startup_dojo.sh
@@ -2,12 +2,14 @@
 #2017-01-10 by Chris Holt (@humor4fun)
 #gets the required bits started for Defect Dojo
 
+#kickoff the nginx server if it is not already started
+sudo service nginx start
+
 #start the server up in a terminal with profile settings set to HOLD the window on process exit
 gnome-terminal --working-directory=/home/dojo/django-DefectDojo --window-with-profile=cronterm --display=:0.0 --execute python manage.py runserver
 
-#wait for the server to start up
+#give django plenty of time to start up
 sleep 5
 
-#open a window to see that you can connect to the server
-#firefox https://127.0.0.1:8000 http://127.0.0.1:8000
-firefox https://your.domain.com http://your.domain.com http://localhost https://localhost http://127.0.0.1:8000
+#run heartbeat tests for django and nginx
+gnome-terminal --working-directory=/home/dojo/ --window-with-profile=cronterm --display=:0.0 --execute ./status-dojo
