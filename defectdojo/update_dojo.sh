8a368b080d9775a3f2bdffb555c0dc7de71f3300
Chris Holt <humor4fun@users.noreply.github.com>
2017-03-09 12:48:14 -0500

    Update update_dojo.sh
    Added 
    ```
    read d < <(curl -s -S https://raw.githubusercontent.com/OWASP/django-DefectDojo/master/setup.py | sed "s/[',> \t]//g" | sed '/install_requires=\[/,/\],/!d;s/install_requires=\[//g;s/\[//g;s/\]//g;s/=.*//1;/dependency_links$/,$d;s/[0-9]\.[0-9]//g' | sed ':a;N;$!ba;s/\n/ /g') && pip install -r $d
    ```
     to fetch a list of python dependencies for dojo and make sure they are all installed and up to date. This *might* not work.

--- defectdojo/update_dojo.sh
+++ defectdojo/update_dojo.sh
@@ -59,6 +59,11 @@ sudo ./manage.py collectstatic --noinput | tee -a "$LOGFILE"
 echo -e "=====================================================\n" >> "$LOGFILE"
 
 echo -e "=====================================================" >> "$LOGFILE"
+echo -e "Installing any new python components:\n" >> "$LOGFILE"
+sudo read d < <(curl -s -S https://raw.githubusercontent.com/OWASP/django-DefectDojo/master/setup.py | sed "s/[',> \t]//g" | sed '/install_requires=\[/,/\],/!d;s/install_requires=\[//g;s/\[//g;s/\]//g;s/=.*//1;/dependency_links$/,$d;s/[0-9]\.[0-9]//g' | sed ':a;N;$!ba;s/\n/ /g') && pip install -r $d | tee -a "$LOGFILE"
+echo -e "=====================================================\n" >> "$LOGFILE"
+
+echo -e "=====================================================" >> "$LOGFILE"
 echo -e "Fetching updated scripts:\n" >> "$LOGFILE"
 echo -e "curl https://raw.githubusercontent.com/humor4fun/bash-scripts/master/defectdojo/update_dojo.sh -o update_dojo\n" >> $LOGFILE
 curl https://raw.githubusercontent.com/humor4fun/bash-scripts/master/defectdojo/update_dojo.sh -o update_dojo | tee -a "$LOGFILE"
