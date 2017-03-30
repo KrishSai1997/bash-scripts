022ddf7ab7a9e4cd0fa6857b9a0ebd9b7a63f16f
Chris Holt <humor4fun@users.noreply.github.com>
2017-03-30 11:57:29 -0400

    Update status_dojo.sh
    Added details to the error states

--- defectdojo/status_dojo.sh
+++ defectdojo/status_dojo.sh
@@ -20,7 +20,7 @@ check_site ()
 		printf "${GREEN}OK${NC}\n"
 		return 1 
 	else
-		printf "${RED}Error\n!!\tRequest on: %s\n!!\tExpected Result: %s\n${NC}" "$2" "$3"
+		printf "${RED}Error\n!!\tRequest on: %s\n!!\tExpected Result: %s\n~~\tThoughts: %s\n${NC}" "$2" "$3" "$4"
 		return 0
 	fi
 }
@@ -28,23 +28,23 @@ check_site ()
 DOMAIN=`cat domain`
 
 # is django responding?
-check_site "1.1" "http://127.0.0.1:8000" "HTTP/1.0 302"
-check_site "1.2" "http://127.0.0.1:8000/login?next=/" "HTTP/1.0 200"
-check_site "1.3" "https://127.0.0.1:8000" "curl: (35)"
-check_site "1.4" "https://127.0.0.1:8000/login?next=/" "curl: (35)"
+check_site "1.1" "http://127.0.0.1:8000" "HTTP/1.0 302" "Django is not running"
+check_site "1.2" "http://127.0.0.1:8000/login?next=/" "HTTP/1.0 200" "Django is not running"
+check_site "1.3" "https://127.0.0.1:8000" "curl: (35)" "HTTPS is ON for Django...shouldn't be"
+check_site "1.4" "https://127.0.0.1:8000/login?next=/" "curl: (35)" "HTTPS is ON for Django...shouldn't be"
 
-check_site "2.1" "http://localhost:8000" "HTTP/1.0 302"
-check_site "2.2" "http://localhost:8000/login?next=/" "HTTP/1.0 200"
-check_site "2.3" "https://localhost:8000" "curl: (35)"
-check_site "2.4" "https://localhost:8000/login?next=/" "curl: (35)"
+check_site "2.1" "http://localhost:8000" "HTTP/1.0 302" "localhost does not resolve"
+check_site "2.2" "http://localhost:8000/login?next=/" "HTTP/1.0 200" "localhost does not resolve"
+check_site "2.3" "https://localhost:8000" "curl: (35)" "HTTPS is ON for Django...shouldn't be"
+check_site "2.4" "https://localhost:8000/login?next=/" "curl: (35)" "HTTPS is ON for Django...shouldn't be"
 
 # is nginx responding?
-check_site "3.1" "http://127.0.0.1" "HTTP/1.1 301"
-check_site "3.2" "http://127.0.0.1/login?next=/" "HTTP/1.1 301"
-check_site "3.3" "https://127.0.0.1" "curl: (51)"
-check_site "3.4" "https://127.0.0.1/login?next=/" "curl: (51)"
+check_site "3.1" "http://127.0.0.1" "HTTP/1.1 301" "nginx not responding with Django pages"
+check_site "3.2" "http://127.0.0.1/login?next=/" "HTTP/1.1 301" "nginx not responding with Django pages"
+check_site "3.3" "https://127.0.0.1" "curl: (51)" "SSL Subject name matches the target hostname...it shouldn't"
+check_site "3.4" "https://127.0.0.1/login?next=/" "curl: (51)" "SSL Subject name matches the target hostname...it shouldn't"
 
-check_site "4.1" "http://"$DOMAIN"" "HTTP/1.1 301"
-check_site "4.2" "http://"$DOMAIN"?next=/" "HTTP/1.1 301"
-check_site "4.3" "https://"$DOMAIN"" "HTTP/1.1 302"
-check_site "4.4" "https://"$DOMAIN"/login?next=/" "HTTP/1.1 200"
+check_site "4.1" "http://"$DOMAIN"" "HTTP/1.1 301" "domain name not configured or DNS not pointing to this server"
+check_site "4.2" "http://"$DOMAIN"?next=/" "HTTP/1.1 301" "domain name not configured or DNS not pointing to this server"
+check_site "4.3" "https://"$DOMAIN"" "HTTP/1.1 302" "domain name not configured or DNS not pointing to this server"
+check_site "4.4" "https://"$DOMAIN"/login?next=/" "HTTP/1.1 200" "domain name not configured or DNS not pointing to this server"
